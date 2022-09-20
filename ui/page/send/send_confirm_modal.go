planetdecred/godcr

replace Toast with modal on multiple pages

ui/page/send/send_confirm_modal.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/planetdecred/godcr/ui/decredmaterial"
 	"github.com/planetdecred/godcr/ui/load"
+	"github.com/planetdecred/godcr/ui/modal"
 	"github.com/planetdecred/godcr/ui/page/components"
 	"github.com/planetdecred/godcr/ui/values"
 )
@@ -72,10 +73,16 @@ func (scm *sendConfirmModal) broadcastTransaction() {
 		scm.isSending = false
 		scm.Modal.SetDisabled(false)
 		if err != nil {
-			scm.Toast.NotifyError(err.Error())
+			errModal := modal.NewErrorModal(scm.Load, err.Error(), func(isChecked bool) bool {
+				return true
+			})
+			scm.ParentWindow().ShowModal(errModal)
 			return
 		}
-		scm.Toast.Notify(values.String(values.StrTxSent))
+		successModal := modal.NewSuccessModal(scm.Load, values.String(values.StrTxSent), func(isChecked bool) bool {
+			return true
+		})
+		scm.ParentWindow().ShowModal(successModal)
 
 		scm.txSent()
 		scm.Dismiss()

ui/page/staking/stake_modal.go
@@ -11,6 +11,7 @@ import (
 	"github.com/planetdecred/dcrlibwallet"
 	"github.com/planetdecred/godcr/ui/decredmaterial"
 	"github.com/planetdecred/godcr/ui/load"
+	"github.com/planetdecred/godcr/ui/modal"
 	"github.com/planetdecred/godcr/ui/page/components"
 	"github.com/planetdecred/godcr/ui/values"
 )
@@ -79,7 +80,10 @@ func (tb *ticketBuyerModal) OnResume() {
 		tbConfig := tb.WL.SelectedWallet.Wallet.AutoTicketsBuyerConfig()
 		acct, err := tb.WL.SelectedWallet.Wallet.GetAccount(tbConfig.PurchaseAccount)
 		if err != nil {
-			tb.Toast.NotifyError(err.Error())
+			errModal := modal.NewErrorModal(tb.Load, err.Error(), func(isChecked bool) bool {
+				return true
+			})
+			tb.ParentWindow().ShowModal(errModal)
 		}
 
 		if tb.WL.SelectedWallet.Wallet.ReadBoolConfigValueForKey(dcrlibwallet.AccountMixerConfigSet, false) &&
@@ -89,7 +93,10 @@ func (tb *ticketBuyerModal) OnResume() {
 		} else {
 			err := tb.accountSelector.SelectFirstWalletValidAccount()
 			if err != nil {
-				tb.Toast.NotifyError(err.Error())
+				errModal := modal.NewErrorModal(tb.Load, err.Error(), func(isChecked bool) bool {
+					return true
+				})
+				tb.ParentWindow().ShowModal(errModal)
 			}
 		}
 
@@ -100,7 +107,10 @@ func (tb *ticketBuyerModal) OnResume() {
 	if tb.accountSelector.SelectedAccount() == nil {
 		err := tb.accountSelector.SelectFirstWalletValidAccount()
 		if err != nil {
-			tb.Toast.NotifyError(err.Error())
+			errModal := modal.NewErrorModal(tb.Load, err.Error(), func(isChecked bool) bool {
+				return true
+			})
+			tb.ParentWindow().ShowModal(errModal)
 		}
 	}
 }
@@ -200,7 +210,10 @@ func (tb *ticketBuyerModal) Handle() {
 		vspHost := tb.vspSelector.SelectedVSP().Host
 		amount, err := strconv.ParseFloat(tb.balToMaintainEditor.Editor.Text(), 64)
 		if err != nil {
-			tb.Toast.NotifyError(err.Error())
+			errModal := modal.NewErrorModal(tb.Load, err.Error(), func(isChecked bool) bool {
+				return true
+			})
+			tb.ParentWindow().ShowModal(errModal)
 			return
 		}
 

crypto-power/cryptopower

ui/page/send/send_confirm_modal.go
@@ -11,6 +11,7 @@ import (
 
 	"gitlab.com/raedah/cryptopower/ui/cryptomaterial"
 	"gitlab.com/raedah/cryptopower/ui/load"
+	"gitlab.com/raedah/cryptopower/ui/modal"
 	"gitlab.com/raedah/cryptopower/ui/page/components"
 	"gitlab.com/raedah/cryptopower/ui/values"
 )
@@ -72,10 +73,16 @@ func (scm *sendConfirmModal) broadcastTransaction() {
 		scm.isSending = false
 		scm.Modal.SetDisabled(false)
 		if err != nil {
-			scm.Toast.NotifyError(err.Error())
+			errModal := modal.NewErrorModal(scm.Load, err.Error(), func(isChecked bool) bool {
+				return true
+			})
+			scm.ParentWindow().ShowModal(errModal)
 			return
 		}
-		scm.Toast.Notify(values.String(values.StrTxSent))
+		successModal := modal.NewSuccessModal(scm.Load, values.String(values.StrTxSent), func(isChecked bool) bool {
+			return true
+		})
+		scm.ParentWindow().ShowModal(successModal)
 
 		scm.txSent()
 		scm.Dismiss()

ui/page/staking/stake_modal.go
@@ -11,6 +11,7 @@ import (
 	"gitlab.com/raedah/cryptopower/libwallet"
 	"gitlab.com/raedah/cryptopower/ui/cryptomaterial"
 	"gitlab.com/raedah/cryptopower/ui/load"
+	"gitlab.com/raedah/cryptopower/ui/modal"
 	"gitlab.com/raedah/cryptopower/ui/page/components"
 	"gitlab.com/raedah/cryptopower/ui/values"
 )
@@ -79,7 +80,10 @@ func (tb *ticketBuyerModal) OnResume() {
 		tbConfig := tb.WL.SelectedWallet.Wallet.AutoTicketsBuyerConfig()
 		acct, err := tb.WL.SelectedWallet.Wallet.GetAccount(tbConfig.PurchaseAccount)
 		if err != nil {
-			tb.Toast.NotifyError(err.Error())
+			errModal := modal.NewErrorModal(tb.Load, err.Error(), func(isChecked bool) bool {
+				return true
+			})
+			tb.ParentWindow().ShowModal(errModal)
 		}
 
 		if tb.WL.SelectedWallet.Wallet.ReadBoolConfigValueForKey(libwallet.AccountMixerConfigSet, false) &&
@@ -89,7 +93,10 @@ func (tb *ticketBuyerModal) OnResume() {
 		} else {
 			err := tb.accountSelector.SelectFirstWalletValidAccount()
 			if err != nil {
-				tb.Toast.NotifyError(err.Error())
+				errModal := modal.NewErrorModal(tb.Load, err.Error(), func(isChecked bool) bool {
+					return true
+				})
+				tb.ParentWindow().ShowModal(errModal)
 			}
 		}
 
@@ -100,7 +107,10 @@ func (tb *ticketBuyerModal) OnResume() {
 	if tb.accountSelector.SelectedAccount() == nil {
 		err := tb.accountSelector.SelectFirstWalletValidAccount()
 		if err != nil {
-			tb.Toast.NotifyError(err.Error())
+			errModal := modal.NewErrorModal(tb.Load, err.Error(), func(isChecked bool) bool {
+				return true
+			})
+			tb.ParentWindow().ShowModal(errModal)
 		}
 	}
 }
@@ -200,7 +210,10 @@ func (tb *ticketBuyerModal) Handle() {
 		vspHost := tb.vspSelector.SelectedVSP().Host
 		amount, err := strconv.ParseFloat(tb.balToMaintainEditor.Editor.Text(), 64)
 		if err != nil {
-			tb.Toast.NotifyError(err.Error())
+			errModal := modal.NewErrorModal(tb.Load, err.Error(), func(isChecked bool) bool {
+				return true
+			})
+			tb.ParentWindow().ShowModal(errModal)
 			return
 		}
 