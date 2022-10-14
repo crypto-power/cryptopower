ac2ab69970163285fdb4b1ddb77254939929c5b4
Justin Do <justin.namdo@gmail.com>
2022-10-14 15:33:24 +0700

    clear code

--- ui/page/privacy/account_mixer_page.go
+++ ui/page/privacy/account_mixer_page.go
@@ -129,7 +129,7 @@ func (pg *AccountMixerPage) bottomSectionLabel(clickable *cryptomaterial.Clickab
 	}
 }
 
-func (pg *AccountMixerPage) MixerProgressBarLayout(gtx C) D {
+func (pg *AccountMixerPage) mixerProgressBarLayout(gtx C) D {
 	totalAmount := (pg.mixedBalance + pg.unmixedBalance).ToCoin()
 	pacentage := (pg.mixedBalance.ToCoin() / totalAmount) * 100
 
@@ -205,7 +205,7 @@ func (pg *AccountMixerPage) mixerHeaderContent() layout.FlexChild {
 							return txt.Layout(gtx)
 						}),
 						layout.Rigid(func(gtx C) D {
-							return layout.Inset{Left: values.MarginPadding16}.Layout(gtx, pg.MixerProgressBarLayout)
+							return layout.Inset{Left: values.MarginPadding16}.Layout(gtx, pg.mixerProgressBarLayout)
 						}),
 					)
 				})
