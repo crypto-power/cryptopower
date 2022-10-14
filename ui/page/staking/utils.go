2d79943c52535e1164d939996d1d19d008be8858
Justin Do <justin.namdo@gmail.com>
2022-10-14 18:17:27 +0700

    fix live ticket not display and clear code

--- ui/page/staking/utils.go
+++ ui/page/staking/utils.go
@@ -4,14 +4,10 @@ import (
 	"fmt"
 	"image"
 	"sort"
-	"strings"
 	"time"
 
 	"gioui.org/layout"
-	"gioui.org/text"
-	"gioui.org/unit"
 
-	"github.com/decred/dcrd/dcrutil/v4"
 	"gitlab.com/raedah/cryptopower/libwallet/wallets/dcr"
 	"gitlab.com/raedah/cryptopower/ui/cryptomaterial"
 	"gitlab.com/raedah/cryptopower/ui/load"
@@ -141,75 +137,6 @@ func stakeToTransactionItems(l *load.Load, txs []dcr.Transaction, newestFirst bo
 	return tickets, nil
 }
 
-func ticketStatusTooltip(gtx C, l *load.Load, tx *transactionItem) D {
-	status := l.Theme.Label(values.TextSize14, strings.ToUpper(tx.status.Title))
-	status.Font.Weight = text.Medium
-	status.Color = tx.status.Color
-
-	maturity := l.WL.SelectedWallet.Wallet.TicketMaturity()
-	blockTime := l.WL.MultiWallet.TargetTimePerBlockMinutes()
-	maturityDuration := time.Duration(maturity*int32(blockTime)) * time.Minute
-
-	var title, mainDesc, subDesc string
-	switch tx.status.TicketStatus {
-	case dcr.TicketStatusUnmined:
-		title = values.String(values.StrUnminedInfo)
-	case dcr.TicketStatusImmature:
-		title = values.StringF(values.StrImmatureInfo, maturity, maturityDuration)
-	case dcr.TicketStatusLive:
-		title = values.String(values.StrLiveInfo)
-		mainDesc = values.String(values.StrLiveInfoDisc)
-		subDesc = values.String(values.StrLiveInfoDiscSub)
-	case dcr.TicketStatusVotedOrRevoked:
-		if tx.ticketSpender.Type == dcr.TxTypeVote {
-			title = values.String(values.StrVotedInfo)
-			mainDesc = values.String(values.StrVotedInfoDisc)
-		} else {
-			title = values.String(values.StrRevokeInfo)
-			mainDesc = values.String(values.StrRevokeInfoDisc)
-		}
-
-		if tx.ticketSpender.Confirmations(l.WL.SelectedWallet.Wallet.GetBestBlock().Height) > maturity {
-			mainDesc = ""
-		} else {
-			mainDesc = values.StringF(mainDesc, maturity, maturityDuration)
-		}
-	case dcr.TicketStatusExpired:
-		title = values.StringF(values.StrExpiredInfo, l.WL.SelectedWallet.Wallet.TicketMaturity())
-		mainDesc = values.String(values.StrExpiredInfoDisc)
-		subDesc = values.String(values.StrExpiredInfoDiscSub)
-	}
-
-	titleLabel := l.Theme.Label(values.TextSize14, title)
-
-	mainDescLabel := l.Theme.Label(values.TextSize14, mainDesc)
-	mainDescLabel.Color = l.Theme.Color.GrayText2
-	subDescLabel := l.Theme.Label(values.TextSize14, subDesc)
-	subDescLabel.Color = l.Theme.Color.GrayText2
-	return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-		layout.Rigid(func(gtx C) D {
-			return layout.Flex{Axis: layout.Horizontal, Alignment: layout.Middle}.Layout(gtx,
-				layout.Rigid(tx.status.Icon.Layout16dp),
-				layout.Rigid(toolTipContent(layout.Inset{Left: values.MarginPadding4}, status.Layout)),
-			)
-		}),
-		layout.Rigid(toolTipContent(layout.Inset{Top: values.MarginPadding8}, titleLabel.Layout)),
-		layout.Rigid(func(gtx C) D {
-			if mainDesc != "" {
-				return toolTipContent(layout.Inset{Top: values.MarginPadding8}, mainDescLabel.Layout)(gtx)
-			}
-
-			return D{}
-		}),
-		layout.Rigid(func(gtx C) D {
-			if subDesc != "" {
-				return toolTipContent(layout.Inset{Top: values.MarginPadding8}, subDescLabel.Layout)(gtx)
-			}
-			return layout.Dimensions{}
-		}),
-	)
-}
-
 func ticketStatusDetails(gtx C, l *load.Load, tx *transactionItem) D {
 	date := time.Unix(tx.transaction.Timestamp, 0).Format("Jan 2, 2006")
 	timeSplit := time.Unix(tx.transaction.Timestamp, 0).Format("03:04:05 PM")
@@ -245,7 +172,8 @@ func ticketStatusDetails(gtx C, l *load.Load, tx *transactionItem) D {
 			}),
 		)
 	case dcr.TicketStatusLive:
-		lbl := l.Theme.Label(values.TextSize16, values.String(values.StrLiveInfoDisc))
+		expiry := l.WL.SelectedWallet.Wallet.TicketExpiry()
+		lbl := l.Theme.Label(values.TextSize16, values.StringF(values.StrLiveInfoDisc, expiry, getTimeToMatureOrExpire(l, tx), expiry))
 		lbl.Color = col
 		return lbl.Layout(gtx)
 	case dcr.TicketStatusVotedOrRevoked:
@@ -288,248 +216,6 @@ func multiContent(gtx C, l *load.Load, leftText, rightText string) D {
 	)
 }
 
-func ticketCardTooltip(gtx C, rectLayout layout.Dimensions, tooltip *cryptomaterial.Tooltip, leftInset unit.Dp, body layout.Widget) {
-	inset := layout.Inset{
-		Top:  values.MarginPadding15,
-		Left: leftInset,
-	}
-
-	rect := image.Rectangle{
-		Max: image.Point{
-			X: rectLayout.Size.X,
-			Y: rectLayout.Size.Y,
-		},
-	}
-
-	tooltip.Layout(gtx, rect, inset, body)
-}
-
-func titleDescTooltip(gtx C, l *load.Load, title string, desc string) layout.Dimensions {
-	titleLabel := l.Theme.Label(values.TextSize14, title)
-	titleLabel.Color = l.Theme.Color.GrayText2
-
-	descLabel := l.Theme.Label(values.TextSize14, desc)
-
-	return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-		layout.Rigid(titleLabel.Layout),
-		layout.Rigid(toolTipContent(layout.Inset{Top: values.MarginPadding4}, descLabel.Layout)),
-	)
-}
-
-func toolTipContent(inset layout.Inset, body layout.Widget) layout.Widget {
-	return func(gtx C) D {
-		return inset.Layout(gtx, body)
-	}
-}
-
-// deprecated to be removed in subsequent code clean uo
-// ticketCard layouts out Stake info with the shadow box, use for list horizontal or list grid
-func ticketCard(gtx layout.Context, l *load.Load, tx *transactionItem, showWalletName bool) layout.Dimensions {
-	wal := l.WL.MultiWallet.DCRWalletWithID(tx.transaction.WalletID)
-	txStatus := tx.status
-
-	// add this data to transactionItem so it can be shared with list
-	maturity := l.WL.SelectedWallet.Wallet.TicketMaturity()
-
-	return cryptomaterial.LinearLayout{
-		Width:       gtx.Dp(values.MarginPadding168),
-		Height:      cryptomaterial.WrapContent,
-		Orientation: layout.Vertical,
-		Shadow:      l.Theme.Shadow(),
-	}.Layout(gtx,
-		layout.Rigid(func(gtx C) D {
-			return cryptomaterial.LinearLayout{
-				Width:      cryptomaterial.MatchParent,
-				Height:     cryptomaterial.WrapContent,
-				Background: txStatus.Background,
-				Border:     cryptomaterial.Border{Radius: cryptomaterial.TopRadius(8)},
-			}.Layout2(gtx, func(gtx C) D {
-				return layout.Stack{}.Layout(gtx,
-					layout.Stacked(func(gtx C) D {
-						gtx.Constraints.Min.X = gtx.Constraints.Max.X
-						return layout.Center.Layout(gtx, func(gtx C) D {
-							return layout.Inset{
-								Top:    values.MarginPadding24,
-								Bottom: values.MarginPadding24,
-							}.Layout(gtx, txStatus.Icon.Layout36dp)
-						})
-					}),
-					layout.Expanded(func(gtx C) D {
-						if !tx.showTime {
-							return D{}
-						}
-
-						return layout.NE.Layout(gtx, func(gtx C) D {
-							timeWrapper := l.Theme.Card()
-							timeWrapper.Radius = cryptomaterial.CornerRadius{TopRight: 8, TopLeft: 0, BottomRight: 0, BottomLeft: 8}
-							return timeWrapper.Layout(gtx, func(gtx C) D {
-								return layout.Inset{
-									Top:    values.MarginPadding4,
-									Bottom: values.MarginPadding4,
-									Right:  values.MarginPadding8,
-									Left:   values.MarginPadding8,
-								}.Layout(gtx, func(gtx C) D {
-
-									confirmations := tx.confirmations
-									if tx.ticketSpender != nil {
-										confirmations = tx.ticketSpender.Confirmations(wal.GetBestBlockHeight())
-									}
-
-									timeRemaining := time.Duration(float64(maturity-confirmations)*l.WL.MultiWallet.TargetTimePerBlockMinutes()) * time.Minute
-									maturityDuration := components.TimeFormat(int(timeRemaining.Seconds()), false)
-									txt := l.Theme.Label(values.TextSize14, maturityTimeFormat(int(timeRemaining.Minutes())))
-
-									durationLayout := layout.Flex{Alignment: layout.Middle}.Layout(gtx,
-										layout.Rigid(func(gtx C) D {
-											return layout.Inset{Right: values.MarginPadding4}.Layout(gtx, l.Theme.Icons.TimerIcon.Layout12dp)
-										}),
-										layout.Rigid(txt.Layout),
-									)
-
-									var durationTitle = values.String(values.StrLiveIn) // immature
-									if txStatus.TicketStatus != dcr.TicketStatusImmature {
-										// voted or revoked
-										durationTitle = values.String(values.StrSpendableIn)
-									}
-
-									ticketCardTooltip(gtx, durationLayout, tx.durationTooltip, values.MarginPadding0, func(gtx C) D {
-										return titleDescTooltip(gtx, l, durationTitle, values.StringF(values.StrDuration, maturityDuration, confirmations, maturity))
-									})
-
-									return durationLayout
-								})
-							})
-						})
-					}),
-					layout.Expanded(func(gtx C) D {
-						return layout.S.Layout(gtx, func(gtx C) D {
-
-							if !tx.showProgress {
-								return D{}
-							}
-
-							p := l.Theme.ProgressBar(int(tx.progress))
-							p.Height = values.MarginPadding4
-							p.Color = txStatus.ProgressBarColor
-							p.TrackColor = txStatus.ProgressTrackColor
-							return p.Layout2(gtx)
-						})
-					}),
-				)
-			})
-		}),
-		layout.Rigid(func(gtx C) D {
-			return cryptomaterial.LinearLayout{
-				Width:       cryptomaterial.MatchParent,
-				Height:      cryptomaterial.WrapContent,
-				Orientation: layout.Vertical,
-				Padding:     layout.UniformInset(values.MarginPadding16),
-			}.Layout(gtx,
-				layout.Rigid(func(gtx C) D {
-					return components.LayoutBalanceWithUnit(gtx, l, dcrutil.Amount(tx.transaction.Amount).String())
-				}),
-				layout.Rigid(func(gtx C) D {
-					return layout.Inset{
-						Top:    values.MarginPadding4,
-						Bottom: values.MarginPadding8,
-					}.Layout(gtx, func(gtx C) D {
-						return layout.Flex{Alignment: layout.Middle}.Layout(gtx,
-							layout.Rigid(func(gtx C) D {
-								txt := l.Theme.Label(values.TextSize14, txStatus.Title)
-								txt.Color = txStatus.Color
-								txt.Font.Weight = text.Medium
-								txtLayout := txt.Layout(gtx)
-								ticketCardTooltip(gtx, txtLayout, tx.statusTooltip, values.MarginPadding0, func(gtx C) D {
-									return ticketStatusTooltip(gtx, l, tx)
-								})
-								return txtLayout
-							}),
-							layout.Rigid(func(gtx C) D {
-								if !showWalletName {
-									return D{}
-								}
-
-								return layout.Inset{
-									Left:  values.MarginPadding4,
-									Right: values.MarginPadding4,
-								}.Layout(gtx, func(gtx C) D {
-									txt := l.Theme.Label(values.TextSize14, "•")
-									txt.Color = l.Theme.Color.GrayText2
-
-									return txt.Layout(gtx)
-								})
-							}),
-							layout.Rigid(func(gtx C) D {
-								if !showWalletName {
-									return D{}
-								}
-
-								txt := l.Theme.Label(values.TextSize14, wal.Name)
-								txt.Color = l.Theme.Color.GrayText2
-								txtLayout := txt.Layout(gtx)
-								ticketCardTooltip(gtx, txtLayout, tx.walletNameTooltip, values.MarginPadding0, func(gtx C) D {
-									return titleDescTooltip(gtx, l, values.String(values.StrWalletName), txt.Text)
-								})
-								return txtLayout
-							}),
-						)
-					})
-				}),
-				layout.Rigid(func(gtx C) D {
-					txt := l.Theme.Label(values.TextSize14, time.Unix(tx.transaction.Timestamp, 0).Format("Jan 2"))
-					txt.Color = l.Theme.Color.GrayText3
-					return layout.Flex{Alignment: layout.Middle}.Layout(gtx,
-						layout.Rigid(func(gtx C) D {
-							txtLayout := txt.Layout(gtx)
-							ticketCardTooltip(gtx, txtLayout, tx.dateTooltip, values.MarginPaddingMinus10, func(gtx C) D {
-								dateTime := time.Unix(tx.transaction.Timestamp, 0).Format("Jan 2, 2006 at 03:04:05 PM")
-								return titleDescTooltip(gtx, l, values.String(values.StrPurchased), dateTime)
-							})
-							return txtLayout
-						}),
-						layout.Rigid(func(gtx C) D {
-
-							var tooltipTitle string
-							if tx.ticketSpender != nil { // voted or revoked
-								if tx.ticketSpender.Type == dcr.TxTypeVote {
-									tooltipTitle = values.String(values.StrDaysToVote)
-								} else {
-									tooltipTitle = values.String(values.StrDaysToMiss)
-								}
-							} else if txStatus.TicketStatus == dcr.TicketStatusImmature ||
-								txStatus.TicketStatus == dcr.TicketStatusLive {
-								tooltipTitle = values.String(values.StrStakeAge)
-							} else {
-								return D{}
-							}
-
-							return layout.Flex{Alignment: layout.Middle}.Layout(gtx,
-								layout.Rigid(func(gtx C) D {
-									return layout.Inset{
-										Left:  values.MarginPadding4,
-										Right: values.MarginPadding4,
-									}.Layout(gtx, func(gtx C) D {
-										ic := cryptomaterial.NewIcon(l.Theme.Icons.ImageBrightness1)
-										return ic.Layout(gtx, values.MarginPadding5)
-									})
-								}),
-								layout.Rigid(func(gtx C) D {
-									txt.Text = tx.ticketAge
-									txtLayout := txt.Layout(gtx)
-									ticketCardTooltip(gtx, txtLayout, tx.daysBehindTooltip, values.MarginPaddingMinus10, func(gtx C) D {
-										return titleDescTooltip(gtx, l, tooltipTitle, tx.ticketAge)
-									})
-									return txtLayout
-								}),
-							)
-						}),
-					)
-				}),
-			)
-		}),
-	)
-}
-
 func ticketListLayout(gtx C, l *load.Load, ticket *transactionItem) layout.Dimensions {
 	return layout.Inset{
 		Right: values.MarginPadding26,
@@ -558,16 +244,6 @@ func ticketListLayout(gtx C, l *load.Load, ticket *transactionItem) layout.Dimen
 				return ticketStatusDetails(gtx, l, ticket)
 			})
 	})
-}
-
-// todo: cleanup
-func createOrderDropDown(th *cryptomaterial.Theme) *cryptomaterial.DropDown {
-	return th.DropDown([]cryptomaterial.DropDownItem{{Text: values.String(values.StrNewest)},
-		{Text: values.String(values.StrOldest)}}, values.StakingDropdownGroup, 1)
-}
-
-func maturityTimeFormat(maturityTimeMinutes int) string {
-	return fmt.Sprintf("%02d:%02d", maturityTimeMinutes/60, maturityTimeMinutes%60)
 }
 
 func nextTicketRemaining(allsecs int) string {
@@ -599,4 +275,19 @@ func nextTicketRemaining(allsecs int) string {
 		str += fmt.Sprintf("%ds ", allsecs)
 	}
 	return str
+}
+
+func getTimeToMatureOrExpire(l *load.Load, tx *transactionItem) int {
+	progressMax := l.WL.SelectedWallet.Wallet.TicketMaturity()
+	if tx.status.TicketStatus == dcr.TicketStatusLive {
+		progressMax = l.WL.SelectedWallet.Wallet.TicketExpiry()
+	}
+
+	confs := tx.transaction.Confirmations(l.WL.SelectedWallet.Wallet.GetBestBlockHeight())
+	if tx.ticketSpender != nil {
+		confs = tx.ticketSpender.Confirmations(l.WL.SelectedWallet.Wallet.GetBestBlockHeight())
+	}
+
+	progress := (float32(confs) / float32(progressMax)) * 100
+	return int(progress)
 }
