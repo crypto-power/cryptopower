1722f9e04bb81d644f2cb1f751902b9dba819ea4
dmigwi <migwindungu0@gmail.com>
2022-11-07 17:24:37 +0300

    Add txparser.go and decodetx.go files

--- libwallet/assets/btc/transactions.go
+++ libwallet/assets/btc/transactions.go
@@ -1,24 +1,17 @@
 package btc
 
 import (
-	"bytes"
-	"encoding/hex"
 	"encoding/json"
-	"fmt"
 	"sync"
 
 	sharedW "code.cryptopower.dev/group/cryptopower/libwallet/assets/wallet"
 	"code.cryptopower.dev/group/cryptopower/libwallet/txhelper"
 	"code.cryptopower.dev/group/cryptopower/libwallet/utils"
-	"github.com/btcsuite/btcd/blockchain"
-	"github.com/btcsuite/btcd/txscript"
-	"github.com/btcsuite/btcd/wire"
-	"github.com/btcsuite/btcutil"
 	"github.com/btcsuite/btcwallet/wallet"
 )
 
-// UnMinedTxHeight defines the height of the txs
-const UnMinedTxHeight int32 = -1
+// UnminedTxHeight defines the block height of the txs in the mempool
+const UnminedTxHeight int32 = -1
 
 // txCache helps to cache the transactions fetched.
 type txCache struct {
@@ -36,7 +29,7 @@ func (asset *BTCAsset) PublishUnminedTransactions() error {
 		return utils.ErrBTCNotInitialized
 	}
 
-	// Triggers the mempool txs updated if they are outdated
+	// Triggers the update of txs in the mempool if they are outdated
 	if _, err := asset.getTransactionsRaw(0, 0, true); err != nil {
 		return err
 	}
@@ -72,7 +65,7 @@ func (asset *BTCAsset) GetTransactionRaw(txHash string) (*sharedW.Transaction, e
 	return nil, err
 }
 
-func (asset *BTCAsset) TxMatchesFilter(tx *sharedW.Transaction, txFilter int32) bool {
+func (asset *BTCAsset) TxMatchesFilter(_ *sharedW.Transaction, txFilter int32) bool {
 	return txhelper.TxDirectionInvalid != asset.btcSupportedTxFilter(txFilter)
 }
 
@@ -161,12 +154,14 @@ func (asset *BTCAsset) getTransactionsRaw(offset, limit int32, newestFirst bool)
 		}
 	}
 
-	refheight := offset + limit
-	if refheight > 0 {
+	// refHeight can be used as the start or endblock height depending on the order
+	// required.
+	refHeight := offset + limit
+	if refHeight > 0 {
 		if newestFirst { // Ascending order
-			endBlock = wallet.NewBlockIdentifierFromHeight(refheight)
+			endBlock = wallet.NewBlockIdentifierFromHeight(refHeight)
 		} else { // Descending Order
-			startBlock = wallet.NewBlockIdentifierFromHeight(refheight)
+			startBlock = wallet.NewBlockIdentifierFromHeight(refHeight)
 		}
 	}
 
@@ -175,7 +170,7 @@ func (asset *BTCAsset) getTransactionsRaw(offset, limit int32, newestFirst bool)
 		return nil, err
 	}
 
-	unminedTxs := asset.decodeTransactionWithTxSummary(UnMinedTxHeight, txResult.UnminedTransactions)
+	unminedTxs := asset.decodeTransactionWithTxSummary(UnminedTxHeight, txResult.UnminedTransactions)
 	minedTxs := asset.extractTxs(txResult.MinedTransactions)
 
 	// Cache the recent data.
@@ -196,136 +191,4 @@ func (asset *BTCAsset) extractTxs(blocks []wallet.Block) []sharedW.Transaction {
 		txs = append(txs, decodedTxs...)
 	}
 	return txs
-}
-
-func (asset *BTCAsset) decodeTransactionWithTxSummary(blockheight int32, txsummary []wallet.TransactionSummary) []sharedW.Transaction {
-	txs := make([]sharedW.Transaction, 0, len(txsummary))
-	for _, rawtx := range txsummary {
-		txHex := fmt.Sprintf("%x", rawtx.Transaction)
-		decodedTx, _ := asset.decodeTxHex(txHex)
-		txSize := decodedTx.SerializeSize()
-
-		//TODO: Check why tx fee returned is zero despite int not being zero on the explorer
-		feeRate := rawtx.Fee * 1000 / btcutil.Amount(txSize)
-
-		// BTC transactions are either coinbase or regular txs.
-		txType := txhelper.TxTypeRegular
-		if blockchain.IsCoinBaseTx(decodedTx) {
-			txType = txhelper.TxTypeCoinBase
-		}
-
-		inputs, totalInputsAmount := asset.decodeTxInputs(decodedTx, rawtx.MyInputs)
-		outputs, totalOutputsAmount := asset.decodeTxOutputs(decodedTx, rawtx.MyOutputs)
-		amount, direction := txhelper.TransactionAmountAndDirection(totalInputsAmount, totalOutputsAmount, int64(rawtx.Fee))
-
-		txs = append(txs, sharedW.Transaction{
-			WalletID:    asset.GetWalletID(),
-			Hash:        rawtx.Hash.String(),
-			Type:        txType,
-			Hex:         txHex,
-			Timestamp:   rawtx.Timestamp,
-			BlockHeight: blockheight,
-
-			Version:  decodedTx.Version,
-			LockTime: int32(decodedTx.LockTime),
-			Fee:      int64(rawtx.Fee),
-			FeeRate:  int64(feeRate),
-			Size:     txSize,
-			Label:    rawtx.Label,
-
-			Direction: direction,
-			Amount:    amount,
-			Inputs:    inputs,
-			Outputs:   outputs,
-		})
-	}
-	return txs
-}
-
-func (asset *BTCAsset) decodeTxHex(txHex string) (*wire.MsgTx, error) {
-	b, err := hex.DecodeString(txHex)
-	if err != nil {
-		return nil, err
-	}
-
-	tx := &wire.MsgTx{}
-	if err = tx.Deserialize(bytes.NewReader(b)); err != nil {
-		return nil, err
-	}
-
-	return tx, nil
-}
-
-func (asset *BTCAsset) decodeTxInputs(mtx *wire.MsgTx,
-	walletInputs []wallet.TransactionSummaryInput) (inputs []*sharedW.TxInput, totalWalletInputs int64) {
-	inputs = make([]*sharedW.TxInput, len(mtx.TxIn))
-
-	for i, txIn := range mtx.TxIn {
-		input := &sharedW.TxInput{
-			PreviousTransactionHash:  txIn.PreviousOutPoint.Hash.String(),
-			PreviousTransactionIndex: int32(txIn.PreviousOutPoint.Index),
-			PreviousOutpoint:         txIn.PreviousOutPoint.String(),
-			AccountNumber:            -1, // correct account number is set below if this is a wallet output
-		}
-
-		// override account details if this is wallet input
-		for _, walletInput := range walletInputs {
-			if int(walletInput.Index) == i {
-				input.AccountNumber = int32(walletInput.PreviousAccount)
-				input.Amount = int64(walletInput.PreviousAmount)
-				break
-			}
-		}
-
-		if input.AccountNumber != -1 {
-			totalWalletInputs += input.Amount
-		}
-
-		inputs[i] = input
-	}
-	return
-}
-
-func (asset *BTCAsset) decodeTxOutputs(mtx *wire.MsgTx,
-	walletOutputs []wallet.TransactionSummaryOutput) (outputs []*sharedW.TxOutput, totalWalletOutput int64) {
-	outputs = make([]*sharedW.TxOutput, len(mtx.TxOut))
-
-	for i, txOut := range mtx.TxOut {
-		// get address and script type for output
-		var address, scriptType string
-		scriptClass, addrs, _, err := txscript.ExtractPkScriptAddrs(txOut.PkScript, asset.chainParams)
-		if err != nil {
-			log.Errorf("Error wheh extracting pkscript")
-		}
-
-		if len(addrs) > 0 {
-			address = addrs[0].String()
-		}
-		scriptType = scriptClass.String()
-
-		output := &sharedW.TxOutput{
-			Index:         int32(i),
-			Amount:        txOut.Value,
-			ScriptType:    scriptType,
-			Address:       address, // correct address, account name and number set below if this is a wallet output
-			AccountNumber: -1,
-		}
-
-		// override address and account details if this is wallet output
-		for _, walletOutput := range walletOutputs {
-			if int32(walletOutput.Index) == output.Index {
-				output.Internal = walletOutput.Internal
-				output.AccountNumber = int32(walletOutput.Account)
-				break
-			}
-		}
-
-		if output.AccountNumber != -1 {
-			totalWalletOutput += output.Amount
-		}
-
-		outputs[i] = output
-	}
-
-	return
 }
