99f3a45c8bc9258634040eb2984927bfb4a86c1a
dmigwi <migwindungu0@gmail.com>
2023-04-12 01:45:41 +0300

    add address management

--- libwallet/assets/ltc/address.go
+++ libwallet/assets/ltc/address.go
@@ -1,29 +1,178 @@
 package ltc
 
-import "code.cryptopower.dev/group/cryptopower/libwallet/utils"
+import (
+	"fmt"
+
+	"code.cryptopower.dev/group/cryptopower/libwallet/utils"
+	"decred.org/dcrwallet/v2/errors"
+	"github.com/ltcsuite/ltcd/ltcutil"
+)
+
+// AddressInfo holds information about an address.
+// If the address belongs to the querying wallet,
+// IsMine will be true and the AccountNumber and
+// AccountName values will be populated
+type AddressInfo struct {
+	Address       string
+	IsMine        bool
+	AccountNumber uint32
+	AccountName   string
+}
 
 // IsAddressValid checks if the provided address is valid.
 func (asset *Asset) IsAddressValid(address string) bool {
-	log.Error(utils.ErrLTCMethodNotImplemented("IsAddressValid"))
-	return false
+	_, err := ltcutil.DecodeAddress(address, asset.chainParams)
+	return err == nil
 }
 
 // HaveAddress checks if the provided address belongs to the wallet.
 func (asset *Asset) HaveAddress(address string) bool {
-	log.Error(utils.ErrLTCMethodNotImplemented("HaveAddress"))
-	return false
+	if !asset.WalletOpened() {
+		return false
+	}
+
+	addr, err := ltcutil.DecodeAddress(address, asset.chainParams)
+	if err != nil {
+		return false
+	}
+
+	have, err := asset.Internal().LTC.HaveAddress(addr)
+	if err != nil {
+		return false
+	}
+
+	return have
+}
+
+// AddressInfo returns information about an address.
+func (asset *Asset) AddressInfo(address string) (*AddressInfo, error) {
+	const op errors.Op = "btc.AddressInfo"
+
+	if !asset.WalletOpened() {
+		return nil, utils.ErrBTCNotInitialized
+	}
+
+	addr, err := ltcutil.DecodeAddress(address, asset.chainParams)
+	if err != nil {
+		return nil, err
+	}
+
+	addressInfo := &AddressInfo{
+		Address: address,
+	}
+	isMine, err := asset.Internal().LTC.HaveAddress(addr)
+	if err != nil {
+		log.Error(op, err)
+	}
+	if isMine {
+		addressInfo.IsMine = isMine
+
+		accountNumber, err := asset.Internal().LTC.AccountOfAddress(addr)
+		if err != nil {
+			return nil, err
+		}
+		addressInfo.AccountNumber = accountNumber
+
+		accountName, err := asset.AccountName(int32(accountNumber))
+		if err != nil {
+			return nil, err
+		}
+		addressInfo.AccountName = accountName
+	}
+
+	return addressInfo, nil
 }
 
 // CurrentAddress gets the most recently requested payment address from the
 // asset. If that address has already been used to receive funds, the next
 // chained address is returned.
 func (asset *Asset) CurrentAddress(account int32) (string, error) {
-	return "", utils.ErrLTCMethodNotImplemented("CurrentAddress")
+	if asset.IsRestored && !asset.ContainsDiscoveredAccounts() {
+		return "", errors.E(utils.ErrAddressDiscoveryNotDone)
+	}
+
+	if !asset.WalletOpened() {
+		return "", utils.ErrBTCNotInitialized
+	}
+
+	addr, err := asset.Internal().LTC.CurrentAddress(uint32(account), asset.GetScope())
+	if err != nil {
+		log.Errorf("CurrentAddress error: %v", err)
+		return "", err
+	}
+	return addr.String(), nil
 }
 
 // NextAddress returns the address immediately following the last requested
 // payment address. If that address has already been used to receive funds,
 // the next chained address is returned.
 func (asset *Asset) NextAddress(account int32) (string, error) {
-	return "", utils.ErrLTCMethodNotImplemented("NextAddress")
+	if asset.IsRestored && !asset.ContainsDiscoveredAccounts() {
+		return "", errors.E(utils.ErrAddressDiscoveryNotDone)
+	}
+
+	if !asset.WalletOpened() {
+		return "", utils.ErrBTCNotInitialized
+	}
+
+	// NewAddress returns the next external chained address for a wallet.
+	address, err := asset.Internal().LTC.NewAddress(uint32(account), asset.GetScope())
+	if err != nil {
+		log.Errorf("NewExternalAddress error: %w", err)
+		return "", err
+	}
+
+	return address.String(), nil
+}
+
+// AccountOfAddress returns the account name of the provided address.
+func (asset *Asset) AccountOfAddress(address string) (string, error) {
+	addr, err := ltcutil.DecodeAddress(address, asset.chainParams)
+	if err != nil {
+		return "", utils.TranslateError(err)
+	}
+
+	if !asset.WalletOpened() {
+		return "", utils.ErrBTCNotInitialized
+	}
+
+	accountNumber, err := asset.Internal().LTC.AccountOfAddress(addr)
+	if err != nil {
+		return "", utils.TranslateError(err)
+	}
+
+	accountName, err := asset.AccountName(int32(accountNumber))
+	if err != nil {
+		return "", err
+	}
+
+	return accountName, nil
+}
+
+// AddressPubKey returns the public key of the provided address.
+func (asset *Asset) AddressPubKey(address string) (string, error) {
+	addr, err := ltcutil.DecodeAddress(address, asset.chainParams)
+	if err != nil {
+		return "", err
+	}
+
+	if !asset.WalletOpened() {
+		return "", utils.ErrBTCNotInitialized
+	}
+
+	isMine, _ := asset.Internal().LTC.HaveAddress(addr)
+	if !isMine {
+		return "", fmt.Errorf("address does not belong to the wallet")
+	}
+
+	pubKey, err := asset.Internal().LTC.PubKeyForAddress(addr)
+	if err != nil {
+		return "", err
+	}
+
+	pubKeyAddr, err := ltcutil.NewAddressPubKey(pubKey.SerializeCompressed(), asset.chainParams)
+	if err != nil {
+		return "", err
+	}
+	return pubKeyAddr.String(), nil
 }
