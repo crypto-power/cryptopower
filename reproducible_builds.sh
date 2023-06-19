b326ccfcc61866c33a2418d8a1c64e4cd0cc71b8
dreacot <kennedyizuegbu@yahoo.com>
2023-06-19 17:55:46 +0100

    exclude any files starting with cryptopower

--- reproducible_builds.sh
+++ reproducible_builds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy files from reproducible-builds to cryptopower
-rsync -av --exclude='README.md' reproduciblebuilds/ .
+rsync -av --exclude='README.md' --exclude='cryptopower*' reproduciblebuilds/ .
 
 # Execute make command
 sudo make "$1"
