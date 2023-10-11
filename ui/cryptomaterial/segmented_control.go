be7b6afa7483074ed2f8b92bc8fd39fe3609b53b
dreacot <kennedyizuegbu@yahoo.com>
2023-10-11 04:22:00 -0600

    cleaup code

--- ui/cryptomaterial/segmented_control.go
+++ ui/cryptomaterial/segmented_control.go
@@ -12,19 +12,19 @@ type SegmentedControl struct {
 	list  *ClickableList
 
 	selectedIndex int
-	segmentItems  []string
+	segmentTitles []string
 
 	changed bool
 }
 
-func (t *Theme) SegmentedControl(segmentItems []string) *SegmentedControl {
+func (t *Theme) SegmentedControl(segmentTitles []string) *SegmentedControl {
 	list := t.NewClickableList(layout.Horizontal)
 	list.IsHoverable = false
 
 	return &SegmentedControl{
-		list:         list,
-		theme:        t,
-		segmentItems: segmentItems,
+		list:          list,
+		theme:         t,
+		segmentTitles: segmentTitles,
 	}
 }
 
@@ -38,14 +38,14 @@ func (sc *SegmentedControl) Layout(gtx C) D {
 		Border:     Border{Radius: Radius(8)},
 	}.Layout(gtx,
 		layout.Rigid(func(gtx C) D {
-			return sc.list.Layout(gtx, len(sc.segmentItems), func(gtx C, i int) D {
+			return sc.list.Layout(gtx, len(sc.segmentTitles), func(gtx C, i int) D {
 				isSelectedSegment := sc.SelectedIndex() == i
 				return layout.Stack{Alignment: layout.Center}.Layout(gtx,
 					layout.Stacked(func(gtx C) D {
 						return layout.Inset{}.Layout(gtx, func(gtx C) D {
 							return layout.Center.Layout(gtx, func(gtx C) D {
 								bg := sc.theme.Color.SurfaceHighlight
-								txt := sc.theme.Label(values.TextSize16, sc.segmentItems[i])
+								txt := sc.theme.Label(values.TextSize16, sc.segmentTitles[i])
 								txt.Color = sc.theme.Color.GrayText1
 								txt.Font.Weight = font.SemiBold
 								border := Border{Radius: Radius(0)}
@@ -77,6 +77,9 @@ func (sc *SegmentedControl) Layout(gtx C) D {
 
 func (sc *SegmentedControl) handleEvents() {
 	if segmentItemClicked, clickedSegmentIndex := sc.list.ItemClicked(); segmentItemClicked {
+		if sc.selectedIndex != clickedSegmentIndex {
+			sc.changed = true
+		}
 		sc.selectedIndex = clickedSegmentIndex
 	}
 }
@@ -86,7 +89,7 @@ func (sc *SegmentedControl) SelectedIndex() int {
 }
 
 func (sc *SegmentedControl) SelectedSegment() string {
-	return sc.segmentItems[sc.selectedIndex]
+	return sc.segmentTitles[sc.selectedIndex]
 }
 func (sc *SegmentedControl) Changed() bool {
 	changed := sc.changed
@@ -95,9 +98,10 @@ func (sc *SegmentedControl) Changed() bool {
 }
 
 func (sc *SegmentedControl) SetSelectedSegment(segment string) {
-	for i, item := range sc.segmentItems {
+	for i, item := range sc.segmentTitles {
 		if item == segment {
 			sc.selectedIndex = i
+			break
 		}
 	}
 }
