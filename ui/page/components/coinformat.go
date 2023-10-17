2b2d6d60544b901179efa3c61ce502b451a45c55
Amos .E <anjeas4@gmail.com>
2023-10-17 14:04:21 +0100

    Remove font weight from format balance

--- ui/page/components/coinformat.go
+++ ui/page/components/coinformat.go
@@ -5,7 +5,6 @@ import (
 	"regexp"
 	"strings"
 
-	"gioui.org/font"
 	"gioui.org/layout"
 	"gioui.org/unit"
 	"github.com/crypto-power/cryptopower/libwallet/utils"
@@ -21,7 +20,7 @@ var (
 	noDecimal                 = regexp.MustCompile(`([0-9]{1,3},*)+`)
 )
 
-func formatBalance(gtx layout.Context, l *load.Load, amount string, mainTextSize unit.Sp, scale float32, col color.NRGBA, displayUnitText bool, fontweight font.Weight) D {
+func formatBalance(gtx layout.Context, l *load.Load, amount string, mainTextSize unit.Sp, scale float32, col color.NRGBA, displayUnitText bool) D {
 
 	startIndex := 0
 	stopIndex := 0
@@ -51,25 +50,18 @@ func formatBalance(gtx layout.Context, l *load.Load, amount string, mainTextSize
 		layout.Rigid(func(gtx C) D {
 			txt := l.Theme.Label(mainTextSize, mainText)
 			txt.Color = col
-			txt.Font.Weight = fontweight
 			return txt.Layout(gtx)
 		}),
 		layout.Rigid(func(gtx C) D {
 			txt := l.Theme.Label(subTextSize, subText)
 			txt.Color = col
-			txt.Font.Weight = fontweight
 			return txt.Layout(gtx)
 		}),
 		layout.Rigid(func(gtx C) D {
 			if displayUnitText {
-				txt := l.Theme.Label(mainTextSize, unitText)
-				txt.Font.Weight = fontweight
-				return txt.Layout(gtx)
+				return l.Theme.Label(mainTextSize, unitText).Layout(gtx)
 			}
-
-			txt := l.Theme.Label(subTextSize, unitText)
-			txt.Font.Weight = fontweight
-			return txt.Layout(gtx)
+			return l.Theme.Label(subTextSize, unitText).Layout(gtx)
 		}),
 	)
 }
@@ -91,25 +83,25 @@ func getIndexUnit(amount string) int {
 // LayoutBalance aligns the main and sub DCR balances horizontally, putting the sub
 // balance at the baseline of the row.
 func LayoutBalance(gtx layout.Context, l *load.Load, amount string) layout.Dimensions {
-	return formatBalance(gtx, l, amount, values.TextSize20, defaultScale, l.Theme.Color.Text, false, font.Normal)
+	return formatBalance(gtx, l, amount, values.TextSize20, defaultScale, l.Theme.Color.Text, false)
 }
 
 func LayoutBalanceWithUnit(gtx layout.Context, l *load.Load, amount string) layout.Dimensions {
-	return formatBalance(gtx, l, amount, values.TextSize20, defaultScale, l.Theme.Color.PageNavText, true, font.Normal)
+	return formatBalance(gtx, l, amount, values.TextSize20, defaultScale, l.Theme.Color.PageNavText, true)
 }
 
 func LayoutBalanceWithUnitSize(gtx layout.Context, l *load.Load, amount string, mainTextSize unit.Sp) layout.Dimensions {
-	return formatBalance(gtx, l, amount, mainTextSize, defaultScale, l.Theme.Color.PageNavText, true, font.Normal)
+	return formatBalance(gtx, l, amount, mainTextSize, defaultScale, l.Theme.Color.PageNavText, true)
 }
 
 func LayoutBalanceSize(gtx layout.Context, l *load.Load, amount string, mainTextSize unit.Sp) layout.Dimensions {
-	return formatBalance(gtx, l, amount, mainTextSize, defaultScale, l.Theme.Color.Text, false, font.Normal)
+	return formatBalance(gtx, l, amount, mainTextSize, defaultScale, l.Theme.Color.Text, false)
 }
 
 func LayoutBalanceSizeScale(gtx layout.Context, l *load.Load, amount string, mainTextSize unit.Sp, scale float32) layout.Dimensions {
-	return formatBalance(gtx, l, amount, mainTextSize, scale, l.Theme.Color.Text, false, font.Normal)
+	return formatBalance(gtx, l, amount, mainTextSize, scale, l.Theme.Color.Text, false)
 }
 
 func LayoutBalanceColor(gtx layout.Context, l *load.Load, amount string, color color.NRGBA) layout.Dimensions {
-	return formatBalance(gtx, l, amount, values.TextSize20, defaultScale, color, false, font.Normal)
+	return formatBalance(gtx, l, amount, values.TextSize20, defaultScale, color, false)
 }
