16ad86f60550d502afd2c1b5a279d90c4afe97a8
Philemon Ukane <ukanephilemon@gmail.com>
2023-12-15 19:12:08 +0100

    add missing change
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>

--- ui/page/info/sync_status.go
+++ ui/page/info/sync_status.go
@@ -14,7 +14,7 @@ import (
 )
 
 func (pg *WalletInfo) initWalletStatusWidgets() {
-	pg.walletStatusIcon = cryptomaterial.NewIcon(pg.Theme.Icons.ImageBrightness1)
+	pg.walletStatusIcon = cryptomaterial.NewIcon(pg.Theme.Icons.DotIcon)
 	pg.syncSwitch = pg.Theme.Switch()
 }
 
