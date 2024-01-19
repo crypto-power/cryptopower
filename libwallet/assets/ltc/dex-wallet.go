480ae096c68ff03f3a3786704d7f8a7a66599068
Philemon Ukane <ukanephilemon@gmail.com>
2024-01-19 23:32:46 +0100

    add tx filter code from upstream
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>

--- libwallet/assets/ltc/dex-wallet.go
+++ libwallet/assets/ltc/dex-wallet.go
@@ -14,6 +14,7 @@ import (
 	"errors"
 	"fmt"
 	"math"
+	"sort"
 	"sync"
 	"time"
 
@@ -476,8 +477,9 @@ func (dw *DEXWallet) EstimateSendTxFee(tx *wire.MsgTx, feeRate uint64, subtract 
 // by this wallet, startTime must be supplied to limit the search. Use the match
 // time assigned by the server.
 // Part of dexbtc.Wallet interface.
-func (dw *DEXWallet) SwapConfirmations(txHash *chainhash.Hash, vout uint32, _ []byte,
-	_ time.Time) (confs uint32, spent bool, err error) {
+func (dw *DEXWallet) SwapConfirmations(txHash *chainhash.Hash, vout uint32, pkScript []byte,
+	startTime time.Time) (confs uint32, spent bool, err error) {
+
 	// First, check if it's a wallet transaction. We probably won't be able
 	// to see the spend status, since the wallet doesn't track the swap contract
 	// output, but we can get the block if it's been mined.
@@ -485,59 +487,299 @@ func (dw *DEXWallet) SwapConfirmations(txHash *chainhash.Hash, vout uint32, _ []
 	if err == nil {
 		return confs, spent, nil
 	}
-
+	var assumedMempool bool
 	switch err {
 	case dexbtc.WalletTransactionNotFound:
 		log.Tracef("swapConfirmations - WalletTransactionNotFound: %v:%d", txHash, vout)
 	case dexbtc.SpentStatusUnknown:
 		log.Tracef("swapConfirmations - SpentStatusUnknown: %v:%d (block %v, confs %d)",
 			txHash, vout, blockHash, confs)
+		if blockHash == nil {
+			// We generated this swap, but it probably hasn't been mined yet.
+			// It's SpentStatusUnknown because the wallet doesn't track the
+			// spend status of the swap contract output itself, since it's not
+			// recognized as a wallet output. We'll still try to find the
+			// confirmations with other means, but if we can't find it, we'll
+			// report it as a zero-conf unspent output. This ignores the remote
+			// possibility that the output could be both in mempool and spent.
+			assumedMempool = true
+		}
 	default:
 		return 0, false, err
 	}
 
+	// Upstream checks a block cache but we don't have that here.
+
+	// Our last option is neutrino.
+	log.Tracef("swapConfirmations - scanFilters: %v:%d (block %v, start time %v)",
+		txHash, vout, blockHash, startTime)
+	utxo, err := dw.scanFilters(txHash, vout, pkScript, startTime, blockHash)
+	if err != nil {
+		return 0, false, err
+	}
+
+	if utxo.spend == nil && utxo.blockHash == nil {
+		if assumedMempool {
+			log.Tracef("swapConfirmations - scanFilters did not find %v:%d, assuming in mempool.",
+				txHash, vout)
+			// NOT asset.CoinNotFoundError since this is normal for mempool
+			// transactions with an SPV wallet.
+			return 0, false, nil
+		}
+		return 0, false, fmt.Errorf("output %s:%v not found with search parameters startTime = %s, pkScript = %x",
+			txHash, vout, startTime, pkScript)
+	}
+
+	if utxo.blockHash != nil {
+		bestHeight, err := dw.getChainHeight()
+		if err != nil {
+			return 0, false, fmt.Errorf("getBestBlockHeight error: %v", err)
+		}
+		confs = uint32(bestHeight) - utxo.blockHeight + 1
+	}
+
+	if utxo.spend != nil {
+		// In the off-chance that a spend was found but not the output itself,
+		// confs will be incorrect here.
+		// In situations where we're looking for the counter-party's swap, we
+		// revoke if it's found to be spent, without inspecting the confs, so
+		// accuracy of confs is not significant. When it's our output, we'll
+		// know the block and won't end up here. (even if we did, we just end up
+		// sending out some inaccurate Data-severity notifications to the UI
+		// until the match progresses)
+		return confs, true, nil
+	}
+
 	// unspent
 	return confs, false, nil
+}
 
-	// // Our last option is neutrino.
-	// log.Tracef("swapConfirmations - scanFilters: %v:%d (block %v, start time %v)",
-	// 	txHash, vout, blockHash, startTime)
-	// utxo, err := w.scanFilters(txHash, vout, pkScript, startTime, blockHash)
-	// if err != nil {
-	// 	return 0, false, err
-	// }
+// scanFilters enables searching for an output and its spending input by
+// scanning BIP158 compact filters. Caller should supply either blockHash or
+// startTime. blockHash takes precedence. If blockHash is supplied, the scan
+// will start at that block and continue to the current blockchain tip, or until
+// both the output and a spending transaction is found. if startTime is
+// supplied, and the blockHash for the output is not known to the wallet, a
+// candidate block will be selected with findBlockTime.
+func (dw *DEXWallet) scanFilters(txHash *chainhash.Hash, vout uint32, pkScript []byte, startTime time.Time, blockHash *chainhash.Hash) (*filterScanResult, error) {
+	// TODO: Check that any blockHash supplied is not orphaned?
 
-	// if utxo.spend == nil && utxo.blockHash == nil {
-	// 	if assumedMempool {
-	// 		log.Tracef("swapConfirmations - scanFilters did not find %v:%d, assuming in mempool.",
-	// 			txHash, vout)
-	// 		// NOT asset.CoinNotFoundError since this is normal for mempool
-	// 		// transactions with an SPV wallet.
-	// 		return 0, false, nil
-	// 	}
-	// 	return 0, false, fmt.Errorf("output %s:%v not found with search parameters startTime = %s, pkScript = %x",
-	// 		txHash, vout, startTime, pkScript)
-	// }
+	// Check if we know the block hash for the tx.
+	var limitHeight int32
+	if blockHash == nil {
+		// No checkpoint and no block hash. Gotta guess based on time.
+		var err error
+		_, limitHeight, err = dw.findBlockForTime(startTime)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		// No checkpoint, but user supplied a block hash.
+		var err error
+		limitHeight, err = dw.GetBlockHeight(blockHash)
+		if err != nil {
+			return nil, fmt.Errorf("error getting height for supplied block hash %s", blockHash)
+		}
+	}
 
-	// if utxo.blockHash != nil {
-	// 	bestHeight, err := dw.GetChainHeight()
-	// 	if err != nil {
-	// 		return 0, false, fmt.Errorf("getBestBlockHeight error: %v", err)
-	// 	}
-	// 	confs = uint32(bestHeight) - utxo.blockHeight + 1
-	// }
+	log.Debugf("Performing cfilters scan for %v:%d from height %d", txHash, vout, limitHeight)
 
-	// if utxo.spend != nil {
-	// 	// In the off-chance that a spend was found but not the output itself,
-	// 	// confs will be incorrect here.
-	// 	// In situations where we're looking for the counter-party's swap, we
-	// 	// revoke if it's found to be spent, without inspecting the confs, so
-	// 	// accuracy of confs is not significant. When it's our output, we'll
-	// 	// know the block and won't end up here. (even if we did, we just end up
-	// 	// sending out some inaccurate Data-severity notifications to the UI
-	// 	// until the match progresses)
-	// 	return confs, true, nil
-	// }
+	// Do a filter scan.
+	utxo, err := dw.filterScanFromHeight(*txHash, vout, pkScript, limitHeight, nil)
+	if err != nil {
+		return nil, fmt.Errorf("filterScanFromHeight error: %w", err)
+	}
+	if utxo == nil {
+		return nil, asset.CoinNotFoundError
+	}
+
+	// If we found a block, let's store a reference in our local database so we
+	// can maybe bypass a long search next time.
+	if utxo.blockHash != nil {
+		log.Debugf("cfilters scan SUCCEEDED for %v:%d. block hash: %v, spent: %v",
+			txHash, vout, utxo.blockHash, utxo.spend != nil)
+	}
+
+	return utxo, nil
+}
+
+const medianTimeBlocks = 11
+var maxFutureBlockTime = 2 * time.Hour // see MaxTimeOffsetSeconds in btcd/blockchain/validate.go
+
+// findBlockForTime locates a good start block so that a search beginning at the
+// returned block has a very low likelihood of missing any blocks that have time
+// > matchTime. This is done by performing a binary search (sort.Search) to find
+// a block with a block time maxFutureBlockTime before matchTime. To ensure
+// we also accommodate the median-block time rule and aren't missing anything
+// due to out of sequence block times we use an unsophisticated algorithm of
+// choosing the first block in an 11 block window with no times >= matchTime.
+func (dw *DEXWallet) findBlockForTime(matchTime time.Time) (*chainhash.Hash, int32, error) {
+	offsetTime := matchTime.Add(-maxFutureBlockTime)
+
+	bestHeight, err := dw.getChainHeight()
+	if err != nil {
+		return nil, 0, fmt.Errorf("getChainHeight error: %v", err)
+	}
+
+	getBlockTimeForHeight := func(height int32) (*chainhash.Hash, time.Time, error) {
+		hash, err := dw.cl.GetBlockHash(int64(height))
+		if err != nil {
+			return nil, time.Time{}, err
+		}
+		header, err := dw.cl.GetBlockHeader(hash)
+		if err != nil {
+			return nil, time.Time{}, err
+		}
+		return hash, header.Timestamp, nil
+	}
+
+	iHeight := sort.Search(int(bestHeight), func(h int) bool {
+		var iTime time.Time
+		_, iTime, err = getBlockTimeForHeight(int32(h))
+		if err != nil {
+			return true
+		}
+		return iTime.After(offsetTime)
+	})
+	if err != nil {
+		return nil, 0, fmt.Errorf("binary search error finding best block for time %q: %w", matchTime, err)
+	}
+
+	// We're actually breaking an assumption of sort.Search here because block
+	// times aren't always monotonically increasing. This won't matter though as
+	// long as there are not > medianTimeBlocks blocks with inverted time order.
+	var count int
+	var iHash *chainhash.Hash
+	var iTime time.Time
+	for iHeight > 0 {
+		iHash, iTime, err = getBlockTimeForHeight(int32(iHeight))
+		if err != nil {
+			return nil, 0, fmt.Errorf("getBlockTimeForHeight error: %w", err)
+		}
+		if iTime.Before(offsetTime) {
+			count++
+			if count == medianTimeBlocks {
+				return iHash, int32(iHeight), nil
+			}
+		} else {
+			count = 0
+		}
+		iHeight--
+	}
+	return dw.btcParams.GenesisHash, 0, nil
+}
+
+// spendingInput is added to a filterScanResult if a spending input is found.
+type spendingInput struct {
+	txHash      chainhash.Hash
+	vin         uint32
+	blockHash   chainhash.Hash
+	blockHeight uint32
+}
+
+// filterScanResult is the result from a filter scan.
+type filterScanResult struct {
+	// blockHash is the block that the output was found in.
+	blockHash *chainhash.Hash
+	// blockHeight is the height of the block that the output was found in.
+	blockHeight uint32
+	// txOut is the output itself.
+	txOut *wire.TxOut
+	// spend will be set if a spending input is found.
+	spend *spendingInput
+	// checkpoint is used to track the last block scanned so that future scans
+	// can skip scanned blocks.
+	checkpoint chainhash.Hash
+}
+
+// / filterScanFromHeight scans BIP158 filters beginning at the specified block
+// height until the tip, or until a spending transaction is found.
+func (dw *DEXWallet) filterScanFromHeight(txHash chainhash.Hash, vout uint32, pkScript []byte, startBlockHeight int32, checkPt *filterScanResult) (*filterScanResult, error) {
+	walletBlock := dw.syncedTo() // where cfilters are received and processed
+	tip := walletBlock.Height
+
+	res := checkPt
+	if res == nil {
+		res = new(filterScanResult)
+	}
+
+search:
+	for height := startBlockHeight; height <= tip; height++ {
+		if res.spend != nil && res.blockHash == nil {
+			log.Warnf("A spending input (%s) was found during the scan but the output (%s) "+
+				"itself wasn't found. Was the startBlockHeight early enough?",
+				dexbtc.NewOutPoint(&res.spend.txHash, res.spend.vin),
+				dexbtc.NewOutPoint(&txHash, vout),
+			)
+			return res, nil
+		}
+		blockHash, err := dw.cl.GetBlockHash(int64(height))
+		if err != nil {
+			return nil, fmt.Errorf("error getting block hash for height %d: %w", height, err)
+		}
+		matched, err := dw.matchPkScript(blockHash, [][]byte{pkScript})
+		if err != nil {
+			return nil, fmt.Errorf("matchPkScript error: %w", err)
+		}
+
+		res.checkpoint = *blockHash
+		if !matched {
+			continue search
+		}
+		// Pull the block.
+		log.Tracef("Block %v matched pkScript for output %v:%d. Pulling the block...",
+			blockHash, txHash, vout)
+		block, err := dw.cl.GetBlock(*blockHash)
+		if err != nil {
+			return nil, fmt.Errorf("GetBlock error: %v", err)
+		}
+		msgBlock := block.MsgBlock()
+
+		// Scan every transaction.
+	nextTx:
+		for _, tx := range msgBlock.Transactions {
+			// Look for a spending input.
+			if res.spend == nil {
+				for vin, txIn := range tx.TxIn {
+					prevOut := &txIn.PreviousOutPoint
+					if prevOut.Hash == txHash && prevOut.Index == vout {
+						res.spend = &spendingInput{
+							txHash:      tx.TxHash(),
+							vin:         uint32(vin),
+							blockHash:   *blockHash,
+							blockHeight: uint32(height),
+						}
+						log.Tracef("Found txn %v spending %v in block %v (%d)", res.spend.txHash,
+							txHash, res.spend.blockHash, res.spend.blockHeight)
+						if res.blockHash != nil {
+							break search
+						}
+						// The output could still be in this block, just not
+						// in this transaction.
+						continue nextTx
+					}
+				}
+			}
+			// Only check for the output if this is the right transaction.
+			if res.blockHash != nil || tx.TxHash() != txHash {
+				continue nextTx
+			}
+			for _, txOut := range tx.TxOut {
+				if bytes.Equal(txOut.PkScript, pkScript) {
+					res.blockHash = blockHash
+					res.blockHeight = uint32(height)
+					res.txOut = txOut
+					log.Tracef("Found txn %v in block %v (%d)", txHash, res.blockHash, height)
+					if res.spend != nil {
+						break search
+					}
+					// Keep looking for the spending transaction.
+					continue nextTx
+				}
+			}
+		}
+	}
+	return res, nil
 }
 
 // confirmations looks for the confirmation count and spend status on a
@@ -635,60 +877,62 @@ func (dw *DEXWallet) Connect(_ context.Context, _ *sync.WaitGroup) (err error) {
 // To match the behavior of the RPC method, even if an output is found, if it's
 // known to be spent, no *wire.TxOut and no error will be returned.
 // Part of dexbtc.Wallet interface.
-func (dw *DEXWallet) GetTxOut(txHash *chainhash.Hash, vout uint32, _ []byte, _ time.Time) (*wire.TxOut, uint32, error) {
+func (dw *DEXWallet) GetTxOut(txHash *chainhash.Hash, vout uint32, pkScript []byte, startTime time.Time) (*wire.TxOut, uint32, error) {
 	// Check for a wallet transaction first
 	txDetails, err := dw.walletTransaction(txHash)
+	var blockHash *chainhash.Hash
 	if err != nil && !errors.Is(err, dexbtc.WalletTransactionNotFound) {
 		return nil, 0, fmt.Errorf("walletTransaction error: %w", err)
 	}
 
-	if txDetails == nil {
-		return nil, 0, nil // wallet has not seen tx
+	if txDetails != nil {
+		spent, found := outputSpendStatus(txDetails, vout)
+		if found {
+			if spent {
+				return nil, 0, nil
+			}
+			if len(txDetails.MsgTx.TxOut) <= int(vout) {
+				return nil, 0, fmt.Errorf("wallet transaction %s doesn't have enough outputs for vout %d", txHash, vout)
+			}
+
+			var confs uint32
+			if txDetails.Block.Height > 0 {
+				tip, err := dw.cl.BestBlock()
+				if err != nil {
+					return nil, 0, fmt.Errorf("BestBlock error: %v", err)
+				}
+				confs = uint32(confirms(txDetails.Block.Height, tip.Height))
+			}
+
+			msgTx := &txDetails.MsgTx
+			if len(msgTx.TxOut) <= int(vout) {
+				return nil, 0, fmt.Errorf("wallet transaction %s found, but not enough outputs for vout %d", txHash, vout)
+			}
+			return msgTx.TxOut[vout], confs, nil
+		}
+		if txDetails.Block.Hash != (chainhash.Hash{}) {
+			blockHash = &txDetails.Block.Hash
+		}
 	}
 
-	spent, found := outputSpendStatus(txDetails, vout)
-	if !found || spent {
+	// We don't really know if it's spent, so we'll need to scan.
+	utxo, err := dw.scanFilters(txHash, vout, pkScript, startTime, blockHash)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	if utxo == nil || utxo.spend != nil || utxo.blockHash == nil {
 		return nil, 0, nil
 	}
 
-	if len(txDetails.MsgTx.TxOut) <= int(vout) {
-		return nil, 0, fmt.Errorf("wallet transaction %s doesn't have enough outputs for vout %d", txHash, vout)
+	tip, err := dw.cl.BestBlock()
+	if err != nil {
+		return nil, 0, fmt.Errorf("BestBlock error: %v", err)
 	}
 
-	var confs uint32
-	if txDetails.Block.Height > 0 {
-		tip, err := dw.cl.BestBlock()
-		if err != nil {
-			return nil, 0, fmt.Errorf("BestBlock error: %v", err)
-		}
-		confs = uint32(confirms(txDetails.Block.Height, tip.Height))
-	}
+	confs := uint32(confirms(int32(utxo.blockHeight), tip.Height))
 
-	msgTx := &txDetails.MsgTx
-	if len(msgTx.TxOut) <= int(vout) {
-		return nil, 0, fmt.Errorf("wallet transaction %s found, but not enough outputs for vout %d", txHash, vout)
-	}
-
-	return msgTx.TxOut[vout], confs, nil
-
-	// We don't really know if it's spent, so we'll need to scan.
-	// utxo, err := dw.scanFilters(txHash, vout, pkScript, startTime, blockHash)
-	// if err != nil {
-	// 	return nil, 0, err
-	// }
-
-	// if utxo == nil || utxo.spend != nil || utxo.blockHash == nil {
-	// 	return nil, 0, nil
-	// }
-
-	// tip, err := dw.cl.BestBlock()
-	// if err != nil {
-	// 	return nil, 0, fmt.Errorf("BestBlock error: %v", err)
-	// }
-
-	// confs := uint32(confirms(int32(utxo.blockHeight), tip.Height))
-
-	// return utxo.txOut, confs, nil
+	return utxo.txOut, confs, nil
 }
 
 // SearchBlockForRedemptions attempts to find spending info for the specified
