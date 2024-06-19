603f7135c63e5516fac922e645dce533c217a7b3
dmigwi <migwindungu0@gmail.com>
2024-06-19 15:40:45 +0200

    Remove AddressReuse from wallet config

--- libwallet/internal/loader/dcr/loader.go
+++ libwallet/internal/loader/dcr/loader.go
@@ -59,7 +59,7 @@ type dcrLoader struct {
 // StakeOptions contains the various options necessary for stake mining.
 type StakeOptions struct {
 	VotingEnabled       bool
-	AddressReuse        bool
+	// AddressReuse        bool
 	VotingAddress       stdaddr.StakeAddress
 	PoolAddress         stdaddr.StakeAddress
 	PoolFees            float64
@@ -163,7 +163,7 @@ func (l *dcrLoader) CreateWatchingOnlyWallet(ctx context.Context, params *loader
 		DB:                      db,
 		PubPassphrase:           params.PubPassphrase,
 		VotingEnabled:           so.VotingEnabled,
-		AddressReuse:            so.AddressReuse,
+		// AddressReuse:            so.AddressReuse,
 		VotingAddress:           so.VotingAddress,
 		PoolAddress:             so.PoolAddress,
 		PoolFees:                so.PoolFees,
@@ -227,7 +227,7 @@ func (l *dcrLoader) CreateNewWallet(ctx context.Context, params *loader.CreateWa
 		DB:                      db,
 		PubPassphrase:           params.PubPassphrase,
 		VotingEnabled:           so.VotingEnabled,
-		AddressReuse:            so.AddressReuse,
+		// AddressReuse:            so.AddressReuse,
 		VotingAddress:           so.VotingAddress,
 		PoolAddress:             so.PoolAddress,
 		PoolFees:                so.PoolFees,
@@ -292,7 +292,7 @@ func (l *dcrLoader) OpenExistingWallet(ctx context.Context, walletID string, pub
 		DB:                      db,
 		PubPassphrase:           pubPassphrase,
 		VotingEnabled:           so.VotingEnabled,
-		AddressReuse:            so.AddressReuse,
+		// AddressReuse:            so.AddressReuse,
 		VotingAddress:           so.VotingAddress,
 		PoolAddress:             so.PoolAddress,
 		PoolFees:                so.PoolFees,
