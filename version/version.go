36a6a0c0d018e2e6b4f6fba62d2b248de31d4f2d
dreacot <kennedyizuegbu@yahoo.com>
2024-09-19 15:28:25 +0100

    increament app version to v2.0.1

--- version/version.go
+++ version/version.go
@@ -26,7 +26,7 @@ const (
 	AppName  string = "cryptopower"
 	AppMajor uint   = 2
 	AppMinor uint   = 0
-	AppPatch uint   = 0
+	AppPatch uint   = 1
 )
 
 // go build -v -ldflags "-X github.com/crypto-power/cryptopower/version.appPreRelease= -X github.com/crypto-power/cryptopower/version.appBuild=`git rev-parse --short HEAD`"
