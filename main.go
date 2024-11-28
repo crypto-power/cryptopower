bb2615c0fd6324c6b7d116f4b9648ada0038f701
dreacot <kennedyizuegbu@yahoo.com>
2024-11-28 11:39:02 +0100

    increment app version to 2.0.3

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ const (
 
 var (
 	// Version is the application version. It is set using the -ldflags
-	Version = "2.0.2"
+	Version = "2.0.3"
 	// BuildDate is the date the application was built. It is set using the -ldflags
 	BuildDate string
 	// BuildEnv is the build environment. It is set using the -ldflags
